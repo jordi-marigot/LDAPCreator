0343a9756b6d0c1f3580d503a6b6ea6923e7ba59
jordi-marigot <98736519+jordi-marigot@users.noreply.github.com>
2022-04-19 12:17:20 +0200

    Update LDAPCreator.py

--- LDAPCreator.py
+++ LDAPCreator.py
@@ -29,7 +29,7 @@ if accio == 1:
             file.write("objectClass: posixGroup" + os.linesep + os.linesep)
             file.write("dn: uid="+userName+",ou="+group+",dc=joviat,dc=cat" + os.linesep)
             file.write("uid: "+uidUser + os.linesep)
-            file.write("uidNumber: 2000"+ str(i) + os.linesep)
+            file.write("uidNumber: 2000"+ str(i+5) + os.linesep)
             file.write("uidNumber: 20001" + os.linesep)
             file.write("cn: "+userName + os.linesep)
             file.write("sn: "+userSecondName + os.linesep)
